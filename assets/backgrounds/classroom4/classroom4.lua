d4b28fe78f0605d0d2d32ef103284aeb6da03711
CeccPro <156588124+CeccPro@users.noreply.github.com>
2025-09-02 21:10:16 -0600

    Add files via upload

---
+++ assets/backgrounds/classroom4/classroom4.lua
@@ -0,0 +1,12 @@
+return {
+    version = "1.0.0",
+    assets = {
+        day1 = "smp_classroom4_day1.png",
+        day2 = "smp_classroom4_day2.png",
+        day3 = "smp_classroom4_day3.png",
+        day4 = "smp_classroom4_day4.png",
+        evening1 = "smp_classroom4_evening1.png",
+        evening2 = "smp_classroom4_evening2.png",
+        night = "smp_classroom4_night.png"
+    }
+}
