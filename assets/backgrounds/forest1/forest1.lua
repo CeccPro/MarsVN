6e91c078e6935f0208bbb1350cab300f613c76a0
CeccPro <156588124+CeccPro@users.noreply.github.com>
2025-09-02 21:11:24 -0600

    Add files via upload

---
+++ assets/backgrounds/forest1/forest1.lua
@@ -0,0 +1,9 @@
+return {
+    version = "1.0",
+    assets = {
+        day1 = "fnc_slope_day1.png",
+        evening = "fnc_slope_evening2.png",
+        night = "fnc_slope_night1.png",
+        night_flashlight = "fnc_slope_night1_flash.png",
+    }
+}
