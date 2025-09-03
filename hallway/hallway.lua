07b76388ce322f55095f716549490d9dff926ce5
CeccPro <156588124+CeccPro@users.noreply.github.com>
2025-09-02 21:12:14 -0600

    Delete hallway directory

--- hallway/hallway.lua
+++
@@ -1,9 +0,0 @@
-return {
-    version = "1.0",
-    assets = {
-        day1 = "smp_hallway_day1.png",
-        day2 = "smp_hallway_day2.png",
-        evening = "smp_hallway_evening1.png",
-        night = "smp_hallway_night1.png",
-    }
-}
